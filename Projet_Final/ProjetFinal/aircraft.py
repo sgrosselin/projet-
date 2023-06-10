7f42f0241a4c9986ea7441a921bb87bad7c75256
sgrosselin <s15.grosselin@gmail.com>
2023-06-10 15:59:12 -0400

    modif classe aircaft et creation lecture base donée

--- Projet_Final/ProjetFinal/aircraft.py
+++ Projet_Final/ProjetFinal/aircraft.py
@@ -1,11 +1,38 @@
 
+
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+
+
+data = pd.read_csv('Base_donnee.csv',index_col=0,delimiter=';',decimal=',',thousands=' ')
+data.columns = ['Modele','Constructeur','Type','Nbre passager','Nbr equipage','Modele moteur','conso cruise','surface alaire','Alt cruise','Vcruise','Range','Ma Cruise','Wing span','WTO','WLA','Wf','We']
+
+
 Cd0 = 0.015
 
 class Aircraft:
-    def __init__(self, name, wing_area, wing_span, Range ):
-        self.name = name  # Surface de l'aire en m^2
-        self.S = wing_area  # Surface de l'aire en m^2
-        self.wing_span = wing_span  # Envergure de l'aile en m
-        self.Range = Range
+    def __init__(self):
+        self.name = str(input('Entrez le modèle de votre avion : ')) # Surface de l'aire en m^2
+        print(self.name)
+        
+        
+    def recherche_avion(self):
+         avion = data[data['Modele'] == self.name ]
+         print(avion)
+         self.constructeur= avion['Constructeur']
+         #print(self.constructeur)
+         
+         return avion
+        
+        
+        
+        
+        
+#        self.S 
+#        self.wing_span = wing_span  # Envergure de l'aile en m
+#        self.Range = Range
+#    
+        
 
 
