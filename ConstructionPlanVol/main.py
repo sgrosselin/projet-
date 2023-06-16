06bdc60f2b8e3113d732b65fd56b6039cdca6a4b
sgrosselin <s15.grosselin@gmail.com>
2023-06-16 14:47:21 -0400

    modif vitesse

--- ConstructionPlanVol/main.py
+++ ConstructionPlanVol/main.py
@@ -6,14 +6,18 @@ Created on Tue Jun 13 16:57:05 2023
 @author: salome
 """
 
-from Modules import Aircraft
+from Modules import Aircraft,Vitesse,Conso
 
 from Modules import Distance, Aeroports
 
 
-#Avion = Aircraft()
-aeroport= Aeroports()
-s=aeroport.distance_aeroports()
-Dist = Distance(40000, s)
-print(Dist.distance_montee())
+avion = Aircraft()
+#aeroport= Aeroports()
+#s=aeroport.distance_aeroports()
+#Dist = Distance(40000, s)
+#print(Dist.distance_montee())
 
+v=Vitesse(avion)
+conso=Conso(avion,v)
+v.vitesse_decollage() 
+#v.vitesse_max()
